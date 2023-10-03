47448171ce5a02ee45c5c614e8113db727411791
Guillaume Ferchaud <guillaumeferchaud@hotmail.fr>
2023-10-03 13:27:46 +0200

    syndic migration

--- src/Migrations/Version20230103181828.php
+++ src/Migrations/Version20230103181828.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
 /**
- * Auto-generated Migration: Please modify to your needs!
+ * Auto-generated Migration
  */
 final class Version20230103181828 extends AbstractMigration
 {
